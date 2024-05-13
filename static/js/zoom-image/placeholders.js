73fbbbed9c7f0e1d7e0b5dcc054ad08feaa6fc7b
Haito4 <>
2024-05-13 10:54:57 +1000

    Baseof changes

--- static/js/zoom-image/placeholders.js
+++ static/js/zoom-image/placeholders.js
@@ -1,10 +1,11 @@
 // Show placeholders for paragraphs
-const paragraphs = Array.prototype.slice.call(document.querySelectorAll('p.placeholder'));
+const paragraphs = [].slice.call(document.querySelectorAll('p.placeholder'))
 
-for (const paragraph of paragraphs) {
-    paragraph.innerHTML = paragraph.textContent
-        .split(' ')
-        .filter(text => text.length > 4)
-        .map(text => `<span class="placeholder__word">${text}</span>`)
-        .join(' ');
-}
+paragraphs.forEach(paragraph => {
+  // eslint-disable-next-line no-param-reassign
+  paragraph.innerHTML = paragraph.textContent
+    .split(' ')
+    .filter(text => text.length > 4)
+    .map(text => `<span class="placeholder__word">${text}</span>`)
+    .join(' ')
+})
